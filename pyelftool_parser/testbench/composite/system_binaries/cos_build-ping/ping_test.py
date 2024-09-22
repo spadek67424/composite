efb98fc6f1ff517e44ee33cc71adf0de184d1243
spadek67424 <spadek67424@gmail.com>
2024-09-22 17:35:45 +0000

    fix the test.

--- pyelftool_parser/testbench/composite/system_binaries/cos_build-ping/ping_test.py
+++ pyelftool_parser/testbench/composite/system_binaries/cos_build-ping/ping_test.py
@@ -5,17 +5,23 @@ import analyzer
 
 class Test_TestFunctionSize(unittest.TestCase):
     def setUp(self):
-        basic_block_flag = 0
-        entry_function = "__cosrt_upcall_entry"
-        path = "./tests.unit_pingpong.global.ping"
-        stub_path = "../../../../../src/components/interface/" + "pong" + "/stubs/stubs.S"
-        self.driver = analyzer.driver(path, entry_function, stub_path, basic_block_flag)
+        self.entry_function = "__cosrt_upcall_entry"
+        self.path = "./tests.unit_pingpong.global.ping"
+        self.stub_path = "../../../../../src/components/interface/" + "pong" + "/stubs/stubs.S"
+        
+    def setupmode(self, basic_block_flag):
+        self.driver = analyzer.driver(self.path, self.entry_function, self.stub_path, basic_block_flag)
+        
+    def teststacksize(self):
+        self.setupmode(0)
         self.driver.run()
-    def test(self):
-        
         result = self.driver.PowerOf2(abs(min(self.driver.parser.stacklist)))
-        assert(12, result)
-        
+        self.assertEqual(12, result)
+    def testbasicblock(self):
+        self.setupmode(1)
+        self.driver.run()
+        result = self.driver.PowerOf2(abs(min(self.driver.parser.stacklist)))
+        self.assertEqual(9, result)
 if __name__ == '__main__':
     unittest.main()
     

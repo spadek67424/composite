ed3ffaca0f0fe8272affe868fd062b867491078e
spadek67424 <spadek67424@gmail.com>
2024-10-23 14:18:58 +0000

    add stack test.

---
+++ tools/pyelftool_parser/testbench/stack_size/stack_allcation.py
@@ -0,0 +1,23 @@
+import unittest   # The test framework
+import sys
+sys.path.append('../../../../tools/pyelftool_parser/src')
+import analyzer
+
+class Test_TestFunctionSize(unittest.TestCase):
+    def setUp(self):
+        self.entry_function = "should_pass"
+        self.path = "./ss.elf"
+        self.stub_path = "../../../../../src/components/interface/init/kernel/stubs.S"
+        self.driver = analyzer.driver(self.path, self.entry_function, self.stub_path)
+
+    def teststacksize(self):
+        self.driver.run()
+        stack_size = self.driver.register.reg["max"]
+        self.assertEqual(-8360, stack_size)
+        result = self.driver.PowerOf2(abs(min(self.driver.parser.stacklist)))
+        self.assertEqual(13, result)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
+    

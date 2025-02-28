a78f00232f4b591e0884859d3db7d6c0936a773c
spadek67424 <spadek67424@gmail.com>
2025-02-28 02:30:28 -0500

    change for invocation.rs

--- src/composer/src/invocations.rs
+++ src/composer/src/invocations.rs
@@ -12,12 +12,69 @@ fn sinvs_generate(id: &ComponentId, s: &SystemState) -> Result<Vec<SInv>, String
     let mut invs = Vec::new();
     let mut errors = String::from("");
     let pydep = s.get_graph(id).py_deps();
+    println!("tttttttt");
+    println!("{:#?}", pydep);
+    let white_list = vec![
+    // "tmrmgr_start".to_string(),
+    // "tmrmgr_evt_get".to_string(),
+    // "tmrmgr_stop".to_string(),
+    // "tmrmgr_create".to_string(),
+    // "tmrmgr_evt_set".to_string(),
+    // "tmrmgr_delete".to_string(),
+    // "__evt_trigger".to_string(),
+    // "__evt_get".to_string(),
+    // "__evt_alloc".to_string(),
+    // "__evt_free".to_string(),
+    // "__evt_add".to_string(),
+    // "__evt_rem".to_string(),
+        // "sched_thd_wakeup".to_string(),
+        // "sched_blkpt_trigger".to_string(),
+        // "sched_blkpt_free".to_string(),
+        // "sched_thd_block".to_string(),
+        // "sched_aep_create_closure".to_string(),
+        // "sched_thd_create_closure".to_string(),
+        // "sched_thd_delete".to_string(),
+        // "sched_blkpt_block".to_string(),
+        // "sched_blkpt_alloc".to_string(),
+        // "sched_thd_param_set".to_string(),
+        // "sched_set_tls".to_string(),
+        // "sched_debug_thd_state".to_string(),
+        // "sched_thd_exit".to_string(),
+        // "sched_thd_yield_to".to_string(),
+        // "sched_get_cpu_freq".to_string(),
+        // "capmgr_create_noop".to_string(),
+        "capmgr_vm_vmcs_create".to_string(),
+        "capmgr_vm_shared_region_create".to_string(),
+        "capmgr_vm_msr_bitmap_create".to_string(),
+        "capmgr_vm_vmcb_create".to_string(),
+        "capmgr_asnd_create".to_string(),
+        "capmgr_vm_comp_create".to_string(),
+        "capmgr_aep_create_thunk".to_string(),
+        "capmgr_initthd_create".to_string(),
+        "capmgr_vm_lapic_access_create".to_string(),
+        "capmgr_vm_shared_kernel_page_create_at".to_string(),
+        "capmgr_shared_kernel_page_create".to_string(),
+        "capmgr_thd_create_ext".to_string(),
+        "capmgr_asnd_rcv_create".to_string(),
+        "capmgr_initaep_create".to_string(),
+        "capmgr_aep_create_ext".to_string(),
+        "capmgr_set_tls".to_string(),
+        "capmgr_vm_vcpu_create".to_string(),
+        "capmgr_vm_lapic_create".to_string(),
+        "capmgr_asnd_key_create".to_string(),
+        "memmgr_shared_page_map_aligned".to_string(),
+        "memmgr_shared_page_allocn".to_string(),
+        "memmgr_map_phys_to_virt".to_string(),
+        "memmgr_shared_page_map".to_string(),
+        "memmgr_shared_page_map_aligned_in_vm".to_string(),
+        "memmgr_heap_page_allocn_aligned".to_string(),
+        "memmgr_virt_to_phys".to_string(),
+
+    ];
     // find each undefined symbol
     for (sname, symbinfo) in s.get_objs_id(id).client_symbs() {
         let mut found = false;
-        if !pydep.contains(&format!("__cosrt_c_{}", sname)) {
-            println!("I am second?");
-            println!("symbinfo: {:?}", symbinfo);
+        if !pydep.contains(&format!("__cosrt_c_{}", sname)) && !pydep.contains(&format!("__cosrt_extern_{}", sname)) && !pydep.contains(sname)  && !sname.contains("init_exit") && !sname.contains("addr_get") { //&& !sname.contains("capmgr") { //&&  !white_list.contains(sname) { // {
             println!("sname is not find in pydep: {}", sname);
             continue;
         }

e10f6f9e6e206d616db2c137e6a3d06b6ce7fe79
spadek67424 <spadek67424@gmail.com>
2025-03-01 05:12:12 -0500

    add hardcode.

--- tools/pyelftool_parser/src/analyzer.py
+++ tools/pyelftool_parser/src/analyzer.py
@@ -65,9 +65,7 @@ hardcode = list(["capmgr_initthd_create",
 "slm_thd_alloc_in",
 "sched_thd_create",
 "sched_thd_wakeup",
-"sched_thd_block",
-"sched_thd_exit",
-"sched_thd_create_closure",])
+"sched_blkpt_trigger"])
 
 
 class parser:
